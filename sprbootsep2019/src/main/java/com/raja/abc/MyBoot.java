6578d965a080bd359147c57d2ba2bb4bec51ffe0
RajaShanmugavel <raja.karupiah@gmail.com>
2019-09-25 19:57:49 +1000

    misc

--- sprbootsep2019/src/main/java/com/raja/abc/MyBoot.java
+++ sprbootsep2019/src/main/java/com/raja/abc/MyBoot.java
@@ -1,4 +1,13 @@
 package com.raja.abc;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MyBoot {
+
+    public static void main(String args[]) {
+        SpringApplication.run(MyBoot.class, args);
+    }
+
 }
